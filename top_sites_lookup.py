4727df3c6789e54e6019ca2034688acc82142d84
Gideon Weissman <gideon.weissman@gmail.com>
2020-09-06 18:41:14 -0400

    moved functions to module url_parsers

--- top_sites_lookup.py
+++ top_sites_lookup.py
@@ -1,14 +1,12 @@
 import easygui
 import os
-import requests
 import csv
-from bs4 import BeautifulSoup
-import dateutil.parser
 from datetime import datetime
 import concurrent.futures
+import url_parsers
 
 # filename = easygui.fileopenbox(msg='Select google analytics csv')
-filename = 'Analytics Frontier Group Exclude Internal Traffic Pages 20200701-20200731 (2).csv'
+filename = 'outputs/Analytics Frontier Group Exclude Internal Traffic Pages 20200701-20200731 (2).csv'
 
 
 fieldnames = ['Page', 'Pageviews', 'UniquePageviews', 'AvgTime', 'Entrances', 'BounceRate', 'PercentExit', 'PageValue']
@@ -16,40 +14,6 @@ fieldnames = ['Page', 'Pageviews', 'UniquePageviews', 'AvgTime', 'Entrances', 'B
 with open(filename, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, fieldnames = fieldnames)
     pages = list(reader)
-
-def parse_report(page):
-    try:
-        url = r'https://frontiergroup.org' + page['Page']
-        r = requests.get(url)
-        html_doc = r.text
-        soup = BeautifulSoup(r.text, 'html.parser')
-        title = soup.title.string
-        title = title[0:title.find(' |')]
-        byline_class = 'field field-name-field-report-byline field-type-text-long field-label-above'
-        byline = soup.find(class_ = byline_class).getText()
-        byline = byline[len('Written by: '):-1]
-        rawdate = soup.find(class_ = 'date-display-single').get('content')
-        dateobj = dateutil.parser.parse(rawdate)
-        datestring = dateobj.strftime(r"%m/%d/%Y")
-        return [page['Pageviews'], byline, datestring, title]
-    except:
-        return [page['Pageviews'], page['Page'], 'ERR', 'ERR']
-
-def parse_blog(page):
-    try:
-        url = r'https://frontiergroup.org' + page['Page']
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        title = soup.title.string
-        title = title[0:title.find(' |')]
-        byline = soup.find(class_ = 'blog-byline').getText()
-        byline = byline[3:byline.find(',')]
-        rawdate = soup.find(class_ = 'date-display-single').get('content')
-        dateobj = dateutil.parser.parse(rawdate)
-        datestring = dateobj.strftime(r"%m/%d/%Y")
-        return [page['Pageviews'], byline, datestring, title]
-    except:
-        return [page['Pageviews'], page['Page'], 'ERR', 'ERR']
 
 blogs = []
 reports = []
@@ -61,7 +25,7 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
         if len(futures) >= 10:
             break
         if '/blogs/' in page['Page']:
-            futures.append(executor.submit(parse_blog, page))
+            futures.append(executor.submit(url_parsers.parse_blog, page))
             print(page['Page'] + ' added')
     for future in concurrent.futures.as_completed(futures):
         blogs.append(future.result())
@@ -73,7 +37,7 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
         if len(futures) >= 10:
             break
         if '/reports/' in page['Page']:
-            futures.append(executor.submit(parse_report, page))
+            futures.append(executor.submit(url_parsers.parse_report, page))
             print(page['Page'] + ' added')
     for future in concurrent.futures.as_completed(futures):
         reports.append(future.result())
--- url_parsers.py
+++ url_parsers.py
@@ -1,25 +1,37 @@
-# turn into module
-
-import requests
 from bs4 import BeautifulSoup
-import dateutil
+import dateutil.parser
+import requests
 
-def parse_blog(blog_url):
+def parse_report(page):
     try:
-        url = r'https://frontiergroup.org' + blog_url
+        url = r'https://frontiergroup.org' + page['Page']
         r = requests.get(url)
         html_doc = r.text
-        soup = BeautifulSoup(html_doc, 'html.parser')
+        soup = BeautifulSoup(r.text, 'html.parser')
         title = soup.title.string
         title = title[0:title.find(' |')]
-        print(title)
-        byline = soup.find(class_ = 'blog-byline').getText()
-        byline = byline[3:byline.find(',')]
-        print(byline)
+        byline_class = 'field field-name-field-report-byline field-type-text-long field-label-above'
+        byline = soup.find(class_ = byline_class).getText()
+        byline = byline[len('Written by: '):-1]
         rawdate = soup.find(class_ = 'date-display-single').get('content')
         dateobj = dateutil.parser.parse(rawdate)
         datestring = dateobj.strftime(r"%m/%d/%Y")
-        print(datestring)
-        return [byline, datestring, title]
+        return [page['Pageviews'], byline, datestring, title]
     except:
-        return ['ERROR', 'ERROR', 'ERROR']
+        return [page['Pageviews'], page['Page'], 'ERR', 'ERR']
+
+def parse_blog(page):
+    try:
+        url = r'https://frontiergroup.org' + page['Page']
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        title = soup.title.string
+        title = title[0:title.find(' |')]
+        byline = soup.find(class_ = 'blog-byline').getText()
+        byline = byline[3:byline.find(',')]
+        rawdate = soup.find(class_ = 'date-display-single').get('content')
+        dateobj = dateutil.parser.parse(rawdate)
+        datestring = dateobj.strftime(r"%m/%d/%Y")
+        return [page['Pageviews'], byline, datestring, title]
+    except:
+        return [page['Pageviews'], page['Page'], 'ERR', 'ERR']
